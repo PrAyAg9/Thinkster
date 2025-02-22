ebdba58e52b1f8220101195953a4ab37e47b353d
Prayag <prayagthakur007@gmail.com>
2025-02-22 19:50:20 +0530

    Initial commit with all project files

---
+++ data/linkData.js
@@ -0,0 +1,185 @@
+const links = [
+    {
+        id: 1,
+        url: '/',
+        name: 'Home'
+    },
+
+    {
+        id: 2,
+        url: '#',
+        name: 'About',
+        submenu: [
+            {
+                id: 1,
+                url: '/about',
+                name: 'About Us'
+            },
+            {
+                id: 2,
+                url: '/affiliation',
+                name: 'Affiliation'
+            },
+            {
+                id: 3,
+                url: '/infrastructure',
+                name: 'Infrastructure'
+            },
+            {
+                id: 4,
+                url: '/admission',
+                name: 'Admission'
+            },
+            {
+                id: 5,
+                url: '/principal-message',
+                name: "Principal's Message"
+            },
+            {
+                id: 6,
+                url: '/mandatory-disclosure',
+                name: 'Mandatory Disclosure'
+            },
+        ]
+    },
+
+    {
+        id: 3,
+        url: '#',
+        name: 'Administration',
+        submenu: [
+            {
+                id: 1,
+                url: '/staff-details',
+                name: 'Staff Details'
+            },
+            {
+                id: 2,
+                url: '/school-committee',
+                name: 'School Committee'
+            },
+            {
+                id: 3,
+                url: '/student-enrol',
+                name: 'Student Enrol'
+            },
+            {
+                id: 4,
+                url: '/fee-scholarship',
+                name: 'Fee and Scholarship'
+            },
+            {
+                id: 5,
+                url: '/teachers-training',
+                name: 'Teachers Training'
+            },
+            {
+                id: 6,
+                url: '/tc-sample',
+                name: 'TC Sample'
+            },
+        ]
+    },
+
+    {
+        id: 4,
+        url: '#',
+        name: 'Academics',
+        submenu: [
+            {
+                id: 1,
+                url: '/school-curriculum',
+                name: 'School Curriculum'
+            },
+            {
+                id: 2,
+                url: '/list-of-books',
+                name: 'List of Books'
+            },
+            {
+                id: 3,
+                url: '/school-at-glance',
+                name: 'School at Glance'
+            },
+            {
+                id: 4,
+                url: '/holidays-list',
+                name: 'Holidays List'
+            },
+            {
+                id: 5,
+                url: '/cbse-result',
+                name: 'CBSE Result'
+            },
+        ]
+    },
+
+    {
+        id: 5,
+        url: '#',
+        name: 'More',
+        submenu: [
+            {
+                id: 1,
+                url: '/curricular-details',
+                name: 'CoCurricular Activities'
+            },
+            {
+                id: 2,
+                url: '/school-activities',
+                name: 'School Activities'
+            },
+            {
+                id: 3,
+                url: '/achievements',
+                name: 'Achievements'
+            },
+            {
+                id: 4,
+                url: '/olympiad',
+                name: 'Olympiad'
+            },
+            {
+                id: 5,
+                url: '/community-services',
+                name: 'Community Services'
+            },
+            {
+                id: 6,
+                url: '/sports',
+                name: 'Sports'
+            },
+            {
+                id: 7,
+                url: '/cbse-activities',
+                name: 'CBSE Activities'
+            },
+        ]
+    },
+
+    {
+        id: 6,
+        url: '#',
+        name: 'Gallery',
+        submenu: [
+            {
+                id: 1,
+                url: '/gallery',
+                name: 'Gallery'
+            },
+            {
+                id: 1,
+                url: '/notice',
+                name: 'Notice'
+            },
+        ]
+    },
+
+    {
+        id: 7,
+        url: '/contact',
+        name: 'Contact Us'
+    },
+]
+
+export default links;
